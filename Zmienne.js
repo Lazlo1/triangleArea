4228f05b75be61385750dcfcb0dbe49cee29cbda
Rafal <lazlo102@gmail.com>
2017-03-22 21:27:38 +0100

    Zmienne

---
+++ Zmienne.js
@@ -0,0 +1,13 @@
+var a = 10,
+	b = 7,
+	value= (a * a) + (2 * a * b) - (b * b)
+console.log(value);
+
+var value = true;
+if (value == true) {
+	console.log('Wynik jest dodatni.');
+} else if (value == false) {
+	console.log('Wynik jest ujemny.');
+} else {
+	console.log('Wynik jest równy 0')
+}
